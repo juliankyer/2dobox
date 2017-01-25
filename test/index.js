8016262e876ff4df293e954735daf00618a9c54c
Julian Kyer <julian.kyer@gmail.com>
2017-01-25 14:55:36 -0700

    Add testing environment

--- test/index.js
+++ test/index.js
@@ -9,16 +9,26 @@
 
 const assert    = require('assert');
 const webdriver = require('selenium-webdriver');
-const test      = require('selenium-webdriver/testing')
+const test      = require('selenium-webdriver/testing');
 
 
-test.describe('testing ideabox',()=>{
-  this.timeout(10000);
-  test.it('should allow me to add a title and a description', ()=>{
-    const driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
+describe('testing ideabox',()=>{
+  let driver
 
+  test.beforeEach(()=> {
+    // this.timeout(10000);
+    driver = new webdriver.Builder()
+                          .forBrowser('chrome')
+                          .build();
     driver.get('http://localhost:8080');
+  });
+
+  // test.afterEach(()=> {
+  //   driver.quit();
+  // });
+
+  test.it('should allow me to add a title', ()=>{
+    const title = driver.findElement({ className: 'idea-title'});
+    title.sendKeys('test test test');
   })
 })
