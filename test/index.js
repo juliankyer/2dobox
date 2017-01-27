dc18e3daa81cac4d33296bdad353dfc9d52ebbd7
Julian Kyer <julian.kyer@gmail.com>
2017-01-26 22:05:44 -0700

    Add additional tests with some pseudo code as outline

--- test/index.js
+++ test/index.js
@@ -75,8 +75,8 @@ describe('testing ideabox', function() {
     const task = driver.findElement({ className: 'todo-task'});
     const button = driver.findElement({ className: 'save-button'});
 
-    title.sendKeys('Test title')
-    task.sendKeys('Test Task')
+    title.sendKeys('Test title');
+    task.sendKeys('Test Task');
     button.click();
     driver.navigate().refresh();
 
@@ -91,14 +91,94 @@ describe('testing ideabox', function() {
     });
   });
 
+  test.it('task should have a default importance of normal', ()=> {
+    const title = driver.findElement({ className: 'todo-title'});
+    const task = driver.findElement({ className: 'todo-task'});
+    const button = driver.findElement({ className: 'save-button'});
+
+    title.sendKeys('Test title');
+    task.sendKeys('Test Task');
+    button.click();
+
+    const qualityText = driver.findElement({ className: 'quality-text'});
+    qualityText.getText().then((copy)=> {
+      assert.equal(copy, 'importance: normal');
+    });
+  });
+
+  test.it('task importance should be upvoteable and persist in local storage', ()=> {
+    const title = driver.findElement({ className: 'todo-title'});
+    const task = driver.findElement({ className: 'todo-task'});
+    const button = driver.findElement({ className: 'save-button'});
+
+    title.sendKeys('Test title');
+    task.sendKeys('Test task');
+    button.click();
+
+    const upVote = driver.findElement({ className: 'upvote'});
+    upVote.click();
+    driver.navigate().refresh();
+
+    const qualityText = driver.findElement({ className: 'quality-text'});
+    qualityText.getText().then((copy)=> {
+      assert.equal(copy, 'importance: high');
+    });
+
+  });
+
+  test.it('task importance should be downvoteable', ()=> {
+    const title = driver.findElement({ className: 'todo-title'});
+    const task = driver.findElement({ className: 'todo-task'});
+    const button = driver.findElement({ className: 'save-button'});
+
+    title.sendKeys('Test title');
+    task.sendKeys('Test task');
+    button.click();
+
+    const downVote = driver.findElement({ className: 'downvote'});
+    downVote.click();
+    driver.navigate().refresh();
+
+    const qualityText = driver.findElement({ className: 'quality-text'});
+    qualityText.getText().then((copy)=> {
+      assert.equal(copy, 'importance: low');
+    });
 
   // test.it('when delete is clicked, the corresponding task should be removed from DOM', ()=> {
+  //   const title = driver.findElement({ className: 'todo-title'});
+  //   const task = driver.findElement({ className: 'todo-task'});
+  //   const button = driver.findElement({ className: 'save-button'});
   //
-  // });
+  //   title.sendKeys('Test title');
+  //   task.sendKeys('Test task');
+  //   button.click();
   //
-  // test.it('when task is deleted, it should not persist on browser refresh', ()=> {
-  //
-  // });
+  //   const delete = driver.findElement({ className: 'delete'});
+  //   delete.click();
+  //   // WHERE DO WE GO FROM HERE?!
+  //   });
+
+  test.it('when task is deleted, it should not persist on browser refresh', ()=> {
+      const title = driver.findElement({ className: 'todo-title'});
+      const task = driver.findElement({ className: 'todo-task'});
+      const button = driver.findElement({ className: 'save-button'});
+
+      title.sendKeys('Test title');
+      task.sendKeys('Test task');
+      button.click();
+
+      const delete = driver.findElement({ className: 'delete'});
+      delete.click();
+      driver.navigate().refresh();
+      // FIGURE OUT HOW TO DETERMINE THAT THE IDEA IS NO LONGER THERE
+    });
+
+    
+
+  });
+
+
+
   //
   // test.it('existing task body and title should be editable, and changes should persist on focus-out/enter', ()=> {
   //
@@ -124,21 +204,7 @@ describe('testing ideabox', function() {
   //
   // });
   //
-  // test.it('task should have a default importance of normal', ()=> {
-  //
-  // });
-  //
-  // test.it('task importance should be upvoteable', ()=> {
-  //
-  // });
-  //
-  // test.it('task importance should be downvoteable', ()=> {
-  //
-  // });
-  //
-  // test.it('task importance should persist on page refresh', ()=> {
-  //
-  // });
+
 
   // PHASE THREE TESTS UNDER HERE
 });
